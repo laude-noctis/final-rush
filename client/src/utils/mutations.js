05abeb2491d123e41d14770473669888895241f5
laude-noctis <lizzybumpers11@gmail.com>
2024-02-27 19:22:20 -0600

    I think I wrote the mutations right

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,50 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql `
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+export const ADD_USER = gql `
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+export const SAVE_BOOK = gql `
+mutation SAVE_BOOK($book: BookInput!) {
+    SAVE_BOOK(book: $book) {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+    }
+`
+
+export const REMOVE_BOOK = gql`
+  mutation REMOVE_BOOK($bookId: ID!) {
+    REMOVE_BOOK(bookId: $bookId) {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+`;
