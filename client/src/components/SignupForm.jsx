f2981a3f36628db2b5027cff23bf26989768f306
laude-noctis <lizzybumpers11@gmail.com>
2024-02-28 16:01:33 -0600

    commit to commit

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -15,7 +15,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [ addUser, { error }] = useMutation(ADD_USER)
+  const [ addUser, { error, data }] = useMutation(ADD_USER)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -33,7 +33,7 @@ const SignupForm = () => {
     }
 
     try {
-      const { token, user } = await addUser(
+      const { data } = await addUser(
         {
           variables: {
             ...userFormData
