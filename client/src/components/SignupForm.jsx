abb231a50dfb7326f078cec21db309c2e742eecb
laude-noctis <lizzybumpers11@gmail.com>
2024-02-27 19:43:12 -0600

    replaced the api function with the mutation - signup

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client'
+
+import { ADD_USER } from '../utils/mutations'
+
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +14,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [ addUser, { error }] = useMutation(ADD_USER)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,7 +33,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await addUser(userFormData);
 
       if (!response.ok) {
         console.error(Error)
